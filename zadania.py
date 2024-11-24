4e0e1e7802b43572e105e838b32d9cb772113bb3
Michalina Janowska <125758811+Michalina03@users.noreply.github.com>
2024-11-24 21:53:24 +0100

    Update

---
+++ zadania.py
@@ -0,0 +1,9 @@
+1. Stwórz globalną listę system_members, klasę Employee z atrybutami name, positiono, automatycznie generującym się id, metodą add_employee która dodaje pracownika do globalnej listy i metadą inf wyswietlającą informacje o pracowniku. Dodaj kolejna klase z metodą tworzącą obiekty klasy Employe z danymi w formacie słownika oraz metodą wczytującą dane pracowników z pliku json tworząca z nich listę obiektów Employee. Stwórz 3 obiekty Employee, zapisz dane pracowników do pliku json, wczytaj dane z pliku i odtwórz obiekty Employee oraz wyświetl dane o pracownikach.
+
+2. Napisz funkcje która znajduje nwd i nww liczb 23,46 następnie napisz funkcje która sprawdzi czy otrzymane wyniki to liczby pierwsze czy nie 
+
+3. Napisz słownik zarządzający danymi ucznia takimi jak imię wiek i oceny. Napisz funkcje wyświetlająca informacje o nim, obliczającą średnia z poszczególnych przedmiotów oraz edytowanie danych o uczniach, edytowanie ich ocen oraz dodawanie ocen. 
+
+4. Napisz funkcje generująca plik tekstowy zawierający liczby ciągu arytmetycznego 
+
+5. Napisz program do którego możemy wprowadzić dowolne zdanie. Niech program wyświetli ile mamy liter k w tym zdaniu.
